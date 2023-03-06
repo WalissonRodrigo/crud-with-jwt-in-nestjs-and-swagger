31d9a67b572a92912e65e4b9d71be938dd231e02
Walisson Rodrigo <walisson.rodrigo@turbi.com.br>
2023-03-05 21:54:44 -0300

    add improves in dtos and new dto to login

--- src/dtos/create-team-member-body.ts
+++ src/dtos/create-team-member-body.ts
@@ -1,13 +1,16 @@
 /* eslint-disable prettier/prettier */
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, Length } from 'class-validator';
 
 export class CreateTeamMemberBody {
+  @ApiProperty({ example: 'John Doe', description: 'The complete name of member of team' })
   @IsNotEmpty({
     message: 'The team member name should not be empty',
   })
   @Length(5, 100)
   name: string;
 
+  @ApiProperty({ example: 'Tecnical enginee of team', description: 'The role within the team that is performed' })
   @IsNotEmpty({
     message: 'The team member function should not be empty',
   })
