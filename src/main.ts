fd665a07eb71dd2e09bee41ee7a26554547fba03
Walisson Rodrigo <walisson.rodrigo@turbi.com.br>
2023-03-06 08:59:37 -0300

    enable cors and running format script

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.use(
     ['/docs', '/docs-json'],
@@ -38,6 +39,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/', app, document);
 
-  await app.listen(3000);
+  await app.listen(5000);
 }
 bootstrap();
